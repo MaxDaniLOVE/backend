eeebf25854dc62c685af85f783eb825b58c08020
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-04-06 17:00:17 +0300

    feat: add editing feature for db

--- controlers/places-controlers.js
+++ controlers/places-controlers.js
@@ -99,23 +99,35 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({place: createdPlace})
 }
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
  
   if (!errors.isEmpty()) {
-    console.log(errors.isEmpty)
     throw new HttpError('Invalids inputs passed', 422)
   }
   const placeId = req.params.pid; // { pid: 'p1' }
   const { title, description } = req.body;
-  const updatePlace = { ...DUMMY_PLACES.find(({id}) => id === placeId) };
-  const placeIdx = DUMMY_PLACES.findIndex(({id}) => id === placeId);
-  updatePlace.title = title;
-  updatePlace.description = description;
+  
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError('Fetching failed', 500);
+    return next(error);
+  }
 
-  DUMMY_PLACES[placeIdx] = updatePlace;
 
-  res.status(200).json({place: updatePlace})
+  place.title = title;
+  place.description = description;
+
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError('Editing failed', 500);
+    return next(error);
+  }
+
+  res.status(200).json({place: place.toObject({ getters: true })})
 }
 
 const deletePlace = (req, res, next) => {
